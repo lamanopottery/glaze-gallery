c7dfcdc711e04fb5b9729327396a3eeae57b483a
Alex Hadley <alexhad6@users.noreply.github.com>
2024-11-04 19:59:16 -0800

    Remove trailing slash and fix env variables

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "server",
+  trailingSlash: "never",
   adapter: cloudflare(),
   integrations: [tailwind()],
 });
