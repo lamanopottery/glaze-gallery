c7e3bf5c06826788e3a97f30d76fdabc6447c796
Alex Hadley <alexhad6@users.noreply.github.com>
2024-11-04 20:16:42 -0800

    Revert trailing slash and environment variables

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,16 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   output: "server",
-  trailingSlash: "never",
   adapter: cloudflare(),
   integrations: [tailwind()],
+  // vite: {
+  //   define: {
+  //     "import.meta.env.GLAZE_GALLERY_STUDIO": JSON.stringify(
+  //       process.env.GLAZE_GALLERY_STUDIO,
+  //     ),
+  //     "import.meta.env.GLAZE_GALLERY_STUDIO_EMAIL": JSON.stringify(
+  //       process.env.GLAZE_GALLERY_STUDIO_EMAIL,
+  //     ),
+  //   },
+  // },
 });
