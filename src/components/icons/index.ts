57e80d4724dce698a8ecef960f98dcbddf29032c
Alex Hadley <alexhad6@users.noreply.github.com>
2024-11-30 13:44:29 -0800

    Fix not food safe icon

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as NotFoodSafeIcon } from "./NotFoodSafeIcon.astro";
+export { default as RunnyIcon } from "./RunnyIcon.astro";
+export { default as BlisterJumpCrawlIcon } from "./BlisterJumpCrawlIcon.astro";
+export { default as IconInParentheses } from "./IconInParentheses.astro";
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as GlazeSelect } from "./GlazeSelect.astro";
+export { default as GlazeTile } from "./GlazeTile.astro";
