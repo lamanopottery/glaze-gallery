9432fedeeef877ea45843aa3ad42a262cdfe95bf
Alex Hadley <alexhad6@users.noreply.github.com>
2025-04-04 21:38:01 -0700

    Add glaze tile filtering

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
 /** Name of the site. */
 export const siteName = "Glaze Gallery";
+
+/** Key for the 1st glaze, used as a URL search parameter. */
+export const glaze1Key = "1st-dip";
+
+/** Key for the 2nd glaze, used as a URL search parameter. */
+export const glaze2Key = "2nd-dip";
