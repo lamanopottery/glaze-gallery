d8ccf564866dffb34990e609e6bf4452a5f55fdc
Alex Hadley <alexhad6@users.noreply.github.com>
2025-04-04 19:32:30 -0700

    Add studio logo and favicons

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
     schema: {
       GLAZE_GALLERY_STUDIO: envField.string({ context: "server", access: "public" }),
       GLAZE_GALLERY_STUDIO_EMAIL: envField.string({ context: "server", access: "public" }),
+      GLAZE_GALLERY_STUDIO_URL: envField.string({ context: "server", access: "public" }),
+      GLAZE_GALLERY_STUDIO_LOGO: envField.string({ context: "server", access: "public" }),
+      GLAZE_GALLERY_STUDIO_FAVICON_32: envField.string({ context: "server", access: "public" }),
+      GLAZE_GALLERY_STUDIO_FAVICON_192: envField.string({ context: "server", access: "public" }),
       GLAZE_GALLERY_IMAGES_URL: envField.string({ context: "server", access: "public" }),
       GLAZE_GALLERY_PASSWORD: envField.string({ context: "server", access: "secret" }),
       GLAZE_GALLERY_JWT_SECRET: envField.string({ context: "server", access: "secret" }),
