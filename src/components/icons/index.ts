05567e76ca79d0473adc7a966405d34532e9bdd4
Alex Hadley <alexhad6@users.noreply.github.com>
2025-04-04 23:28:58 -0700

    Add tile flipping

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,4 @@ export { default as NotFoodSafeIcon } from "./NotFoodSafeIcon.astro";
 export { default as RunnyIcon } from "./RunnyIcon.astro";
 export { default as BlisterJumpCrawlIcon } from "./BlisterJumpCrawlIcon.astro";
 export { default as IconInParentheses } from "./IconInParentheses.astro";
+export { default as FlipIcon } from "./FlipIcon.astro";
