993413e6031cf4a081924a62eb21af5e332d5154
Alex Hadley <alexhad6@users.noreply.github.com>
2026-06-16 20:12:52 -0700

    Obfuscate email

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as Header } from "./Header.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as GlazeSelect } from "./GlazeSelect.astro";
 export { default as GlazeTile } from "./GlazeTile.astro";
+export { default as ObfuscatedEmail } from "./ObfuscatedEmail.astro";
 export { default as WarningIcons } from "./WarningIcons.astro";
